7b3ad3300b339d0b36f21e25433317e2a6ef26b3
wilvers <juan.ignacio.wilvers@gmail.com>
2023-06-02 09:09:35 -0300

    nav

--- components/ui/Navbar.jsx
+++ components/ui/Navbar.jsx
@@ -2,13 +2,13 @@ import Link from 'next/link'
 import React from 'react'
 
 export const Navbar = () => {
-  return (
-    <>
-    <ul>
-        <li> <Link href={"/"}>Home</Link> </li>
-        <li> <Link href={"/about"}>about</Link> </li>
-        <li> <Link href={"/contact"}>contact</Link> </li>
-    </ul>
-    </>
-  )
+    return (
+        <nav>
+            <ul>
+                <li> <Link href={"/"}>Home</Link> </li>
+                <li> <Link href={"/about"}>about</Link> </li>
+                <li> <Link href={"/contact"}>contact</Link> </li>
+            </ul>
+        </nav>
+    )
 }
