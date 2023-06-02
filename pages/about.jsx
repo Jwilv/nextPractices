c504625015b6dc64a4fbb4047a57cb55aa499a96
wilvers <juan.ignacio.wilvers@gmail.com>
2023-06-02 18:50:39 -0300

    init getLayaout

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,19 +2,29 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
-import {MainLayout} from '../components/layouts'
+import { MainLayout } from '../components/layouts'
+import { DarkLayout } from '../components/layouts/DarkLayout'
 
 export default function AboutPage() {
     return (
         <>
-            <MainLayout>
-                <div className={styles.description}>
-                    <p>
-                        ve a
-                        <code className={styles.code}> <Link href={"/"}>/home</Link> </code>
-                    </p>
-                </div>
-            </MainLayout>
+            <div className={styles.description}>
+                <p>
+                    ve a
+                    <code className={styles.code}> <Link href={"/"}>/home</Link> </code>
+                </p>
+            </div>
         </>
+    )
+}
+
+AboutPage.getLayaout = function (page) {
+
+    return (
+        <MainLayout>
+            <DarkLayout>
+                {page}
+            </DarkLayout>
+        </MainLayout>
     )
 }
